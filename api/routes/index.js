8cbc388781546cec667f805c91fac0f67eabe208
alexandt <alexandt96@gmail.com>
2021-04-24 00:22:13 -0500

    query params added

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 var got = require('got')
 var axios = require('axios')
-var _ = require('lodash')
+var _ = require('lodash');
+var moment = require('moment')
 
 // I have working calls in insomnia, just need to pull in all the data, find a way to iterate through "all" guilds - or at least a lot of them 
 // Maybe there is a way to get a list of guilds and their ids that have killed at least 1 mythic boss
@@ -16,9 +17,9 @@ var _ = require('lodash')
 // profit
 
 const difficultyToNum = {
-  'mythic': 5,
-  'heroic': 4,
-  'normal': 3,
+  'Mythic': 5,
+  'Heroic': 4,
+  'Normal': 3,
 }
 
 const numToDifficulty = _.invert(difficultyToNum)
@@ -59,65 +60,45 @@ router.get('/test', function(req, res, next) {
         }
    })
 
-   router.get('/cookies', (req, res) => {
-    console.log('Cookies: ', req.headers.cookie)
-    res.send('done')
-   })
 
-   router.get('/test-wcl', async (req, res) => {
-     const wclToken = req.headers.cookie
-     const token = wclToken.split('=')[1]
-     const url = 'https://www.warcraftlogs.com/api/v2/client'
-     const authorization = `Bearer ${token}`
-     const options = { 
-       method: 'GET',
-       headers:{
-         'content-type': 'application/json',
-         authorization
-        },
-       url
-     }
-
-     const response = await axios(options)
-     console.log(response)
-     const {data} = response
-     res.send(data)
-   })
-
-   router.get('/thought-pulls', async (req, res) => {
+   router.get('/pulls/:guildName/:server-:region/:difficulty?', async (req, res) => {
     const wclToken = req.headers.cookie
     const token = wclToken.split('=')[1]
     const url = 'https://www.warcraftlogs.com/api/v2/client/'
     const authorization = `Bearer ${token}`
-    const options = {
-      method: 'post',
-      headers: {authorization},
-      data: {query: `
-      query{
-        reportData{
-          reports(
-            guildName:"Thought", 
-            guildServerSlug:"sargeras", 
-            guildServerRegion:"US"
-            page: 1,      
-            startTime: 1607411048000
 
-          ){
-            has_more_pages,
-            total,
-            data{
+    const {guildName, server, region, difficulty} = req.params
+    const guildServerSlug = _.snakeCase(server)
+    const selectedDifficulty = difficulty ? isNaN(difficulty) ? difficultyToNum[difficulty] : Number(difficulty) : 5
+    const data = {query: `
+    query{
+      reportData{
+        reports(
+          guildName: "${guildName}", 
+          guildServerSlug: "${guildServerSlug}", 
+          guildServerRegion:"${region}"
+          page: 1,      
+          startTime: 1607411048000
+
+        ){
+          data{
+            startTime,
+            endTime,
+            fights{
+              difficulty,
+              kill,
               startTime,
-              fights{
-                difficulty,
-                kill,
-                startTime,
-                name
-              }
+              name
             }
           }
         }
-      }`
-      },
+      }
+    }`
+    }
+    const options = {
+      method: 'post',
+      headers: {authorization},
+      data,
       url
     }
     try{
@@ -127,7 +108,7 @@ router.get('/test', function(req, res, next) {
         return report.fights.map(fight => ({...fight, time: report.startTime + fight.startTime}))
       }))
       const raidFights = fightsWithTimes.filter(fight => fight.difficulty)
-      const fightsByBoss = _.groupBy(raidFights, (fight) => `${fight.name}-${fight.difficulty}`)
+      const fightsByBoss = _.groupBy(raidFights, (fight) => `${fight.name} ${numToDifficulty[fight.difficulty]}`)
       const firstKillPerBoss = _.entries(fightsByBoss).reduce((acc, cur) => {
         const [bossKey, fights] = cur
         const killsForCur = fights.filter(fight => fight.kill)
@@ -135,15 +116,17 @@ router.get('/test', function(req, res, next) {
         acc.push({bossKey, ...firstKill})
         return acc
       }, [])
-      const mythicKills = firstKillPerBoss.filter(kill => kill.difficulty === 5)
 
-      const pullsPerKill = mythicKills.map(kill => {
+      const killsAtSelectedDifficulty = firstKillPerBoss.filter(kill => kill.difficulty === selectedDifficulty)
+
+      const pullsPerKill = killsAtSelectedDifficulty.map(kill => {
         const wipes = fightsByBoss[kill.bossKey].filter(fight => fight.time < kill.time)
-        return {...kill, numWipes: wipes.length}
+        return {...kill, numWipes: wipes.length, firstKilled: moment(kill.time)}
       })
-      res.send(pullsPerKill)
+      const sortedPullCount = pullsPerKill.sort((a,b) => a.time - b.time)
+      res.send(sortedPullCount)
     } catch (e) { 
-      console.log(e)
+      console.error(e)
       res.send(e)
     }
     
