9d1f48b7382e80041ea26ed67430f05ec1fc50a5
alexandt <alexandt96@gmail.com>
2021-04-23 23:29:53 -0500

    pulling thought data

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var got = require('got')
 var axios = require('axios')
+var _ = require('lodash')
 
 // I have working calls in insomnia, just need to pull in all the data, find a way to iterate through "all" guilds - or at least a lot of them 
 // Maybe there is a way to get a list of guilds and their ids that have killed at least 1 mythic boss
@@ -13,6 +14,14 @@ var axios = require('axios')
 // pop up a UI and let the people go to town!
 // ?? 
 // profit
+
+const difficultyToNum = {
+  'mythic': 5,
+  'heroic': 4,
+  'normal': 3,
+}
+
+const numToDifficulty = _.invert(difficultyToNum)
 
 router.get('/test', function(req, res, next) {
      res.json([{
@@ -62,7 +71,10 @@ router.get('/test', function(req, res, next) {
      const authorization = `Bearer ${token}`
      const options = { 
        method: 'GET',
-       headers:{authorization},
+       headers:{
+         'content-type': 'application/json',
+         authorization
+        },
        url
      }
 
@@ -70,6 +82,71 @@ router.get('/test', function(req, res, next) {
      console.log(response)
      const {data} = response
      res.send(data)
+   })
+
+   router.get('/thought-pulls', async (req, res) => {
+    const wclToken = req.headers.cookie
+    const token = wclToken.split('=')[1]
+    const url = 'https://www.warcraftlogs.com/api/v2/client/'
+    const authorization = `Bearer ${token}`
+    const options = {
+      method: 'post',
+      headers: {authorization},
+      data: {query: `
+      query{
+        reportData{
+          reports(
+            guildName:"Thought", 
+            guildServerSlug:"sargeras", 
+            guildServerRegion:"US"
+            page: 1,      
+            startTime: 1607411048000
+
+          ){
+            has_more_pages,
+            total,
+            data{
+              startTime,
+              fights{
+                difficulty,
+                kill,
+                startTime,
+                name
+              }
+            }
+          }
+        }
+      }`
+      },
+      url
+    }
+    try{
+      const response = await axios(options)
+      const {data} = response
+      const fightsWithTimes = _.flatMap(data.data.reportData.reports.data, (report => {
+        return report.fights.map(fight => ({...fight, time: report.startTime + fight.startTime}))
+      }))
+      const raidFights = fightsWithTimes.filter(fight => fight.difficulty)
+      const fightsByBoss = _.groupBy(raidFights, (fight) => `${fight.name}-${fight.difficulty}`)
+      const firstKillPerBoss = _.entries(fightsByBoss).reduce((acc, cur) => {
+        const [bossKey, fights] = cur
+        const killsForCur = fights.filter(fight => fight.kill)
+        const firstKill = _.minBy(killsForCur, kill => kill.time)
+        acc.push({bossKey, ...firstKill})
+        return acc
+      }, [])
+      const mythicKills = firstKillPerBoss.filter(kill => kill.difficulty === 5)
+
+      const pullsPerKill = mythicKills.map(kill => {
+        const wipes = fightsByBoss[kill.bossKey].filter(fight => fight.time < kill.time)
+        return {...kill, numWipes: wipes.length}
+      })
+      res.send(pullsPerKill)
+    } catch (e) { 
+      console.log(e)
+      res.send(e)
+    }
+    
    })
    
 module.exports = router;
